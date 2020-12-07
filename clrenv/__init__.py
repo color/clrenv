ca4c29d93d4ce373e1fa0483450d8ad60eeaf550
David Cain <david.cain@color.com>
2020-12-07 10:12:43 -0800

    Dramatically speed up YAML parsing for `setenv.sh`
    If available, this change makes YAML parsing use C bindings from LibYAML.
    This is strongly recommended by the PyYAML team, since LibYAML bindings
    are "much faster than the pure Python version."
    
    This change will speed up the `./setenv.sh` script considerably: on my
    system, `mk_shell_variables.py` decreases runtime from 450ms down to
    70ms. That script takes the overwhelming majority of execution time for
    `setenv.sh`.
    
    On Ubuntu systems, `LibYAML` can be used by installing from apt:
    
        apt install libyaml-dev
    
    Homebrew users can run:
    
        brew install libyaml
    
    After doing so, `pyyaml` should be re-installed.
    
    This change preserves the safe loading of files, which should *always*
    be done when loading any untrusted YAML (and is good practice even if
    the source is trusted). The change also doesn't *require* that one have
    the C bindings.

--- clrenv/__init__.py
+++ clrenv/__init__.py
@@ -1,12 +1,13 @@
 import yaml
 
 from .lazy_env import get_env, LazyEnv
+from .load import safe_load
 from .path import find_environment_path
 
 
 def mapping():
     with open(find_environment_path()) as f:
-        return yaml.safe_load(f.read())['mapping']
+        return safe_load(f.read())['mapping']
 
 env = LazyEnv()
 get_env = get_env
