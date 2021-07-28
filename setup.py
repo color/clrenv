5e06ce5cd46998e7460e181423fff7cd7100a355
David Cain <david.cain@color.com>
2021-07-28 09:41:21 -0700

    Fix `ResourceWarning: unclosed file`
    Using `open()` without explicitly closing the handle is not a best
    practice. Use `open()` as a context manager so that handles will be
    closed immediately after their contents are read.
    
    This silences a warning that appears when running with
    `PYTHONWARNINGS=always` (or `python -Wd ...`)

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
-import os
-
 try:
-  from setuptools import setup
+    from setuptools import setup
 except:
-  from distutils.core import setup
+    from distutils.core import setup
 
 
 setup(name = "clrenv",
-      version = "0.1.8",
+      version = "0.1.9",
       description = "A tool to give easy access to environment yaml file to python.",
       author = "Color Genomics",
       author_email = "dev@getcolor.com",
