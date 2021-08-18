2891ee5ad951b8371882f5e09e17a88e411815d6
Michael Cusack <michael.cusack@color.com>
2021-08-18 10:37:15 -0700

    Format repo using black

--- setup.py
+++ setup.py
@@ -4,16 +4,17 @@ except:
     from distutils.core import setup
 
 
-setup(name = "clrenv",
-      version = "0.1.9",
-      description = "A tool to give easy access to environment yaml file to python.",
-      author = "Color Genomics",
-      author_email = "dev@getcolor.com",
-      url = "https://github.com/ColorGenomics/clrenv",
-      packages = ["clrenv"],
-      install_requires=[
-        'PyYAML>=4.2b1',
-        'munch>=2.2.0',
-      ],
-      license = "MIT",
-      )
+setup(
+    name="clrenv",
+    version="0.1.9",
+    description="A tool to give easy access to environment yaml file to python.",
+    author="Color Genomics",
+    author_email="dev@getcolor.com",
+    url="https://github.com/ColorGenomics/clrenv",
+    packages=["clrenv"],
+    install_requires=[
+        "PyYAML>=4.2b1",
+        "munch>=2.2.0",
+    ],
+    license="MIT",
+)
