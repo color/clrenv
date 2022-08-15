e1de24562fc81fb2c7677321670fc21ea13e9f87
Michael Cusack <80846125+mpcusack-color@users.noreply.github.com>
2022-08-15 09:07:06 -0700

    Revert "Mark clrenv as having type hints"

--- setup.py
+++ setup.py
@@ -5,15 +5,12 @@ requirements = ["PyYAML>=4.2", "boto3>=1.15.18"]
 
 setup(
     name="clrenv",
-    version="0.2.2",
+    version="0.2.4",
     description="A tool to give easy access to environment yaml file to python.",
     author="Color",
     author_email="dev@getcolor.com",
     url="https://github.com/color/clrenv",
     packages=["clrenv"],
-    package_data={
-        'clrenv': ['py.typed'],
-    },
     install_requires=requirements,
     setup_requires=["pytest-runner"],
     tests_require=requirements + ["pytest", "pytest-cov"],
