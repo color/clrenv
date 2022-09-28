9e9ca252592e811b75f3252219dc10f0d0d0b837
Michael Cusack <michael.cusack@color.com>
2022-09-27 22:41:30 -0700

    Enable type checking

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ requirements = ["PyYAML>=4.2", "boto3>=1.15.18"]
 
 setup(
     name="clrenv",
-    version="0.2.4",
+    version="0.3.0",
     description="A tool to give easy access to environment yaml file to python.",
     author="Color",
     author_email="dev@getcolor.com",
     url="https://github.com/color/clrenv",
     packages=["clrenv"],
+    package_data={
+        'clrenv': ['py.typed'],
+    },
     install_requires=requirements,
     setup_requires=["pytest-runner"],
     tests_require=requirements + ["pytest", "pytest-cov"],
